1c2df7c2f9d13e3e23b61eba990e83a3153e37bd
unknown <alexanderdinsmore@yahoo.com>
2024-05-23 16:56:10 -0400

    sidebar_tweakv1

--- recepie-book/src/componets/Sidebar.jsx
+++ recepie-book/src/componets/Sidebar.jsx
@@ -3,6 +3,18 @@ function Sidebar (){
     <div className="sidebar">
   <a className="active" href="#home">Home</a>
   <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  
 </div>
 
     )
